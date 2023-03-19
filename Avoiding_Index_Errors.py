5c75f5e6766e70227e8f5fe66cd8fde260495c71
Sumair Ali <sumairalishahani1@gmail.com>
2023-03-19 17:18:56 +0500

    Add files via upload

---
+++ Avoiding_Index_Errors.py
@@ -0,0 +1,5 @@
+names_lists=['sumair','basit','masood']
+
+# print(names_lists[3])
+# above print  statement gives us the IndentationError because of 
+# the index 3 is out of range of the list
---
+++ organizing_list.py
@@ -0,0 +1,34 @@
+# 3-8. Seeing the World:
+places=['Badshahi Mosque','Mohn jo Daro','Lahore','Islamabad','kotri']
+
+for place in places:
+    print(place)
+
+print(sorted(places))
+print(places)
+
+print(sorted(places,reverse=True))
+print(places)
+
+places.reverse()
+print(places)
+places.reverse()
+print(places)
+
+places.sort()
+print(places)
+places.sort(reverse=True)
+print(places)
+
+# 3-10. Every Function:
+countries_list=['pakistan','india','germany','china','america']
+print(countries_list)
+sorted(countries_list)
+print(countries_list)
+
+countries_list.reverse()
+print(countries_list)
+countries_list.sort()
+print(countries_list)
+countries_list.sort(reverse=True)
+print(countries_list)
